2a44f0ebb37bbc981941d9c6e6efd9037c297764
Natacha Key <nataliaklueva@hotmail.com>
2023-05-17 17:44:17 +0200

    more comments added

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const MongoDBStore = require('connect-mongodb-session')(session);
 
+//created new collection in mongo db
 var store = new MongoDBStore({
   uri: process.env.MONGO_URI,
   collection: 'sessions',
@@ -38,8 +39,13 @@ const User = mongoose.model(
 );
 
 const app = express();
+
+//set the directory where views/templates are located. __dirname is a special variable in Node.js that represents the current directory path of the script file. 
+// we are telling Express to look for views/templates in the current directory.
 app.set('views', __dirname);
+//set the view engine to be used for rendering the views. In this case, it's set to 'ejs', which stands for Embedded JavaScript OR js code in our HTML templates that generate dynamic content.
 app.set('view engine', 'ejs');
+//in other words with these two lines we're telling Express to look for views in the current directory and use EJS as the templating engine for rendering those views
 
 //setting up the LocalStrategy
 passport.use(
@@ -62,10 +68,16 @@ passport.use(
   })
 );
 
+//serialize (converting a complex data structure (such as a user object) into a format that can be easily stored and retrieved)
+//here only the user.id property should be used for serialization. This means that when the user is authenticated, 
+//only their id value will be stored in the session, rather than the entire user object.
+//=when the user is authenticated, only their id value will be stored in the session, rather than the entire user object.
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
+//When subsequent requests are made by the same user, the serialized user identifier is used to retrieve the full user object from the session through deserialization.
+//f the user is successfully found, the user object is passed as the second argument to the done function, indicating successful deserialization. we pass 'null' as an argument- when there is no error
 passport.deserializeUser(async function (id, done) {
   try {
     const user = await User.findById(id);
@@ -84,10 +96,16 @@ app.use(
   })
 );
 
+// sets up Passport to work with Express and prepares it for authentication handling.
 app.use(passport.initialize());
+//enables Passport to restore authentication state from the session, allowing authenticated users to stay logged in across multiple requests.
 app.use(passport.session());
+//parse URL-encoded data from incoming requests to handle form submissions that send data in the application/x-www-form-urlencoded format.
+//extended: false option indicates that the URL-encoded data should be parsed using the built-in querystring library of Node.js
 app.use(express.urlencoded({ extended: false }));
+//in other words express.urlencoded() middleware enables our application to handle URL-encoded form data.
 
+// sets up a variable currentUser in the res.locals object, making it available in our views for convenient access to the currently authenticated user information.
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   next();
